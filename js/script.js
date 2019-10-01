39da7cae57d408252da99b1e6d60497a79c5126d
athleticcoder <56014058+athleticcoder@users.noreply.github.com>
2019-10-01 13:36:36 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,22 @@
-$( '#topheader .navbar-nav a' ).on( 'click', function () {
-	$( '#topheader .navbar-nav' ).find( 'a.active' ).removeClass( 'active' );
-	$( this ).addClass( 'active' );
-});
+$(document).ready(function(){
+	$( '#topheader .navbar-nav a' ).on( 'click', function (event) {
+		$( '#topheader .navbar-nav' ).find( 'a.active' ).removeClass( 'active' );
+		$( this ).addClass( 'active' );
+	});
 
+	$("#topheader .navbar-nav a").on('click', function(event) {
+	  console.log(this.hash);
+	  if (this.hash !== "") {
+		event.preventDefault();
+  
+		var hash = this.hash;
+  
+		$('html, body').animate({
+		  scrollTop: $(hash).offset().top
+		}, 800, function(){
+	 
+		window.location.hash = hash;
+		});
+	  }
+	});
+  });
